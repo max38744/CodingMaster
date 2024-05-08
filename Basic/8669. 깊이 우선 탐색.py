ed2a5e28aa25611bc47bdb34a28c80095fbaa981
max38744 <max38744@naver.com>
2024-05-08 16:38:55 +0900

    set up

---
+++ Basic/8669. 깊이 우선 탐색.py
@@ -0,0 +1,112 @@
+'''
+깊이 우선 탐색이란 그래프를 탐색(또는 순회)하는 방법 중 하나입니다. 
+
+깊이 우선 탐색은 다음과 같은 방식으로 이루어집니다. 
+
+1. 정점을 하나 정하여 방문하였다고 표시합니다. 
+2. 이 정점과 간선으로 연결된 다른 정점에 대하여, 
+2-1. 이미 방문했던 정점이라면 무시합니다. 
+2-2. 아직 방문하지 않은 정점이라면 그 정점부터 다시 1을 적용합니다. 
+
+이를 의사코드로 작성하면 다음과 같게 됩니다. 
+
+function DFS(V, E, C) {  // V : 정점의 집합, E : 간선의 집합, C : 현재 방문할 정점
+  visited[C] = true  // 현재 정점을 방문하였다고 표시
+  for each x in E(C) {   // E(C) : C와 인접한 정점
+    if (!visited[x]) DFS(V, E, x)  // 아직 방문하지 않은 정점이면 해당 정점부터 깊이 우선 탐색
+  }
+}
+
+방향성과 가중치가 없는 그래프가 주어졌을 때 정점 1에서부터 깊이 우선 탐색을 수행하는 프로그램을 작성하세요. 
+
+탐색은 인접한 정점 중에서 정점번호가 작은 정점부터 수행합니다.
+정점의 개수가 N개일 때 각 정점의 번호는 1부터 N까지입니다.
+------------------------------------------------------------------------
+[입력값 설명]
+『첫째 줄에 정점의 개수 N과 간선의 개수 M이 입력됩니다. (1≤N, 0≤M≤1,000)
+둘째 줄부터 M + 1번째 줄까지 정수 A와 B가 입력됩니다. (1 ≤ A, B ≤ 1,000) 이 때 각 줄은 A와 B가 서로 연결되었음을 의미합니다.』
+
+[출력값 설명]
+『정점 1에서 깊이 우선 탐색을 수행한 결과, 거쳐간 노드를 공백으로 구분하여 출력합니다.(1도 출력합니다.)』
+------------------------------------------------------------------------
+예제 입력1
+3 0
+
+예제 출력1
+1
+
+예제 입력2
+5 3
+1 4
+2 3
+4 5
+
+예제 출력2
+1 4 5
+'''
+# -*- coding: utf-8 -*-
+import sys
+input = sys.stdin.readline
+
+# DFS 선언
+def DFS(v, Edges, visited):
+    # 일단 방문한 정점은 저장
+    answer.append(v)
+    # 방문 체크
+    visited[v] = True
+    # 하나씩 체크하자
+    for n in Edges[v]:
+        if(not visited[n]): DFS(n, Edges, visited)
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    # 간선 저장할 배열 선언
+    Edges = [[] for _ in range(N+1)]
+    # 간선 입력받고, 각 인덱스에서 연결된 간선 저장
+    for _ in range(M):
+        A, B = map(int, input().split())
+        Edges[A].append(B)
+        Edges[B].append(A)
+    # 정점번호가 작은 정점부터 수행
+    for edge in Edges:
+        edge.sort()
+    # 방문 기록할 배열 선언
+    visited = [False] * (N+1)
+    # 정답이 될 간선을 순차적으로 저장해둘 배열
+    answer = []
+    # DFS 딸깎
+    DFS(1, Edges, visited)
+    # 정답 출력
+    print(*answer)
+    
+# # Stack으로 구현하려고 했는데 실패함, 어디선가 계속 잘못된 부분이 있다!
+# from collections import deque
+
+# if __name__ == "__main__":
+#     N, M = map(int, input().split())
+#     # 간선 저장할 배열 선언
+#     Edges = [[] for _ in range(N+1)]
+#     # 간선 입력받고, 각 인덱스에서 연결된 간선 저장
+#     for _ in range(M):
+#         A, B = map(int, input().split())
+#         Edges[A].append(B)
+#         Edges[B].append(A)
+#     # 정점번호가 작은 정점부터 수행
+#     for edge in Edges:
+#         edge.sort()
+#     # 방문 기록할 배열 선언
+#     visited = [False] * (N+1)
+    
+#     # Stack 생성
+#     Stack = deque()
+#     # Stack에 첫번째 정점 넣기
+#     Stack.append(1)
+#     # 정점을 순서대로 넣을 리스트 생성
+#     answer = []
+#     while Stack:
+#         v = Stack.pop()
+#         answer.append(v)
+#         visited[v] = True
+#         for n in Edges[v]:
+#             if(not visited[n]): Stack.append(n)
+#     print(*answer)
